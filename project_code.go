47ba5f04b5b0f1fa3e32671d01f92a607a47cd22
Cedrick <irambonacedric55@gmail.com>
2024-09-14 00:52:05 -0500

    Created all the variables for each question in the project

--- project_code.go
+++ project_code.go
@@ -6,3 +6,40 @@ import (
   "time"
   "os/exec"
 )
+
+var(  
+	answer, age string
+	anthem int
+	//age string
+	bones int
+	choice string 
+	covid string
+	c0py string
+	coin int
+	cartier int
+	CFtemp int
+	email string
+	fabric string
+	Fname string
+	gas string
+	geography string
+	grade int
+	iron string
+	invasion int
+	justin string
+	jobs int
+	plusone int
+	province string
+	pi float32
+	physics string
+	Sname string
+	speed string
+	small string
+	sharks string
+	teeth int
+	num int
+	Nitrogen int
+	holocaust string
+	//For Keeping a count to how many questions the user has got correct while doing the quiz
+	questionscorrect int = 0; 
+	)
